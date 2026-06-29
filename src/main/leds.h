b4b5d49d8ec0f050e5ae31141362ef2cd730d6d9
blckmn <blckmn@betaflight.com>
2026-06-29 20:04:44 +0000

    Add the default Betaflight licence header to all source files
    Prepend the standard GPLv3 header (DEFAULT_LICENSE.md) to every C/H file
    under src/main, and emit it from gen_vcp_ids.py so the generated header
    carries it too.

--- src/main/leds.h
+++ src/main/leds.h
@@ -1,3 +1,24 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Status LEDs, all optional and board-defined (see board.h):
 //   BOARD_WIFI_LED_GPIO  - plain GPIO LED, blink cadence reflects WiFi state
 //   BOARD_RGB_LED_GPIO   - WS2812 NeoPixel, colour reflects FC/OTG-USB comms
--- src/main/tls_cert.h
+++ src/main/tls_cert.h
@@ -1,3 +1,24 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Self-signed TLS server credentials for the HTTPS/WSS endpoint.
 //
 // On first use an EC (P-256) self-signed certificate is generated and stored in
