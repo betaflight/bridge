58b13f79928e26242699956e260cd8d4eaaf454a
blckmn <blckmn@users.noreply.github.com>
2026-06-30 20:39:03 +1000

    Add the default licence header to all source files (#6)

--- src/main/ota.h
+++ src/main/ota.h
@@ -1,3 +1,24 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Over-the-air firmware update via HTTP upload. Registers POST /update on the
 // existing web server: the request body is the raw .bin, streamed straight into
 // the inactive OTA slot. On success the boot partition is switched and the
